c27e634555a9ef8641dc6ab0840053daf3549ae7
mikfar <45609856+mikfar@users.noreply.github.com>
2019-02-19 14:08:23 +0000

    more variables sir

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 
 local money = 0
 local sleep = 100
+local hygeine = 100 
+local hunger = 100 
 
 local moneyText = display.newText( money, display.contentCenterX, 20, native.systemFont, 40 )
 moneyText:setFillColor( 5, 0, 0 )
