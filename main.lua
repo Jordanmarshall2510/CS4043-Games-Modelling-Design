b5039829a93e1cb6fdaaf2eb4e4b0864d5d9df4b
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2019-02-19 14:00:30 +0000

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local money = 0
+local sleep = 100
+
+local moneyText = display.newText( money, display.contentCenterX, 20, native.systemFont, 40 )
+moneyText:setFillColor( 5, 0, 0 )
