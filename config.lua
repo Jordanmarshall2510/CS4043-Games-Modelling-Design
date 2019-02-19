8ca337a8ab792d13a7d9e06d4fdb2eba968912b3
mikfar <45609856+mikfar@users.noreply.github.com>
2019-02-19 14:22:03 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,8 +7,11 @@ application =
 {
 	content =
 	{
-		width = 320,
-		height = 480, 
+		--[[ Set this to any widescreen resolution if you want our game 
+		to be widescreen e.g. 1024x768, keep in mind that we need to be careful 
+		with the res of the assets as well --]]
+		width = 1024,
+		height = 768, 
 		scale = "letterbox",
 		fps = 60,
 		
