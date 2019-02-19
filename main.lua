377a31aa630843c9025238005914efd7a01e6480
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2019-02-19 14:10:39 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,5 +9,6 @@ local sleep = 100
 local hygeine = 100 
 local hunger = 100 
 
+
 local moneyText = display.newText( money, display.contentCenterX, 20, native.systemFont, 40 )
-moneyText:setFillColor( 5, 0, 0 )
+moneyText:setFillColor( 0, 0, 0 )
