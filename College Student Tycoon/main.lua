5e76720bb2fb815f90644aef0fa98980fb724946
mikfar <45609856+mikfar@users.noreply.github.com>
2019-02-22 18:58:10 +0000

    Update main.lua

--- College Student Tycoon/main.lua
+++ College Student Tycoon/main.lua
@@ -3,6 +3,9 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+local physics = require "physics"
+physics.start( )
+physics.setGravity( 0, 0 ) -- prevents the character from falling off 
 
 local money = 0
 local sleep = 100
@@ -17,6 +20,8 @@ grass.y = display.contentCenterY
 local library = display.newImageRect("library.png", 74, 87)
 library.x = -222
 library.y = 412
+local libraryOutline = graphics.newOutline( 2, "library.png" )
+
 
 local student = display.newImageRect( "student.png", 70, 70 )
 student.x = display.contentCenterX
@@ -39,9 +44,27 @@ moneyText:setFillColor( 1, 1, 1 )
 
 -----Moves sprite to coords of touch-----
 local function onTouch(event)
-if(event.phase == "ended") then 
-transition.to(student, {x=event.x, y=event.y})
-print(event.x, event.y)
-end
+	if(event.phase == "ended") then 
+	transition.to(student, {x=event.x, y=event.y})
+	print(event.x, event.y)
+	end
 end
 Runtime:addEventListener("touch", onTouch)
+
+--Character physics and building physics-- 
+--[TODO: Make it so the building is rock solid and the character can't go through it at all]-- 
+physics.addBody( student, "dynamic", { bounce=0, angle=0 })
+student.isFixedRotation = true 
+physics.addBody( library, "static")
+
+--Random events, only an example
+local function randomEvent() 
+	--eventID will be added as a random variable
+		if(eventID == 1) --Night out, for instance
+			then 
+			selfEsteem = selfEsteem + 70 -- I propose renaming self esteem to sanity or happiness  
+			money = money - 20 -- I'll add some randomisation to how much you lose
+			sleep = sleep - 20
+			hygeine = hygeine -25
+		end
+	end
