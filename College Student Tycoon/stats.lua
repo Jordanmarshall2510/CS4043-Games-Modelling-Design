b37ad42d0cd77fef75fb7fa54e0e5f6a37b5e917
mikfar <45609856+mikfar@users.noreply.github.com>
2019-03-12 13:18:16 +0000

    Added loop to refresh stats constantly

--- College Student Tycoon/stats.lua
+++ College Student Tycoon/stats.lua
@@ -24,37 +24,29 @@ stats.selfEsteem = 100
 --Display everything, to be called in the main file-- 
 
 function stats.display()
-	
-	stats.moneyText = display.newText("Money " .. stats.money, 500, 20, native.systemFont , 20 )
-	stats.moneyText:setFillColor( 1, 1, 1 )
+	while ( true ) 
+	do
+		stats.moneyText = display.newText("Money " .. stats.money, 500, 20, native.systemFont , 20 )
+		stats.moneyText:setFillColor( 1, 1, 1 )
 
-	stats.sleepText = display.newText("Sleep " .. stats.sleep, 500, 40, native.systemFont , 20 )
-	stats.sleepText:setFillColor( 1, 1, 1 )
+		stats.sleepText = display.newText("Sleep " .. stats.sleep, 500, 40, native.systemFont , 20 )
+		stats.sleepText:setFillColor( 1, 1, 1 )
 
-	stats.hygeineText = display.newText("Hygeine " .. stats.hygeine, 500, 60, native.systemFont , 20 )
-	stats.hygeineText:setFillColor( 1, 1, 1 )
+		stats.hygeineText = display.newText("Hygeine " .. stats.hygeine, 500, 60, native.systemFont , 20 )
+		stats.hygeineText:setFillColor( 1, 1, 1 )
 
-	stats.hungerText = display.newText("Hunger " .. stats.hunger, 500, 80, native.systemFont , 20 )
-	stats.hungerText:setFillColor( 1, 1, 1 )
+		stats.hungerText = display.newText("Hunger " .. stats.hunger, 500, 80, native.systemFont , 20 )
+		stats.hungerText:setFillColor( 1, 1, 1 )
 
-	stats.selfEsteemText = display.newText("Self Esteem " .. stats.selfEsteem, 500, 100, native.systemFont , 20 )
-	stats.moneyText:setFillColor( 1, 1, 1 )
-	
-end
-
-function stats.clear() 
-
-	stats.moneyText:removeSelf()
-	stats.hungerText:removeSelf()
-	stats.sleepText:removeSelf()
-	stats.hygeineText:removeSelf()
-	stats.selfEsteemText:removeSelf() 
-	
-end
-
-function stats.refresh()
-	stats.clear()
-	stats.display()
+		stats.selfEsteemText = display.newText("Self Esteem " .. stats.selfEsteem, 500, 100, native.systemFont , 20 )
+		stats.moneyText:setFillColor( 1, 1, 1 )
+		
+		stats.moneyText:removeSelf()
+		stats.hungerText:removeSelf()
+		stats.sleepText:removeSelf()
+		stats.hygeineText:removeSelf()
+		stats.selfEsteemText:removeSelf() 
+	end
 end
 
 return stats 
