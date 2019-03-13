75875da1fcece6acb16189530a88a572b2d4ee7f
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-03-13 16:38:37 +0000

    Buldings and Physics in
    No Assets folder the physics wont work when in a different folder to the ,ain 1 and we just want a working game first then we can fix it and make it neat

--- College Student Tycoon/main.lua
+++ College Student Tycoon/main.lua
@@ -6,6 +6,7 @@
 local physics = require "physics"
 local events = require "events"
 local stats = require "stats" 
+local assets = require "assets"
 
 physics.start( )
 physics.setGravity( 0, 0 )
@@ -17,9 +18,7 @@ grass.y = display.contentCenterY
 --Intiially display stats, use stats.refresh from now on
 stats.display()
 
-local library = display.newImageRect("library.png", 74, 87)
-library.x = -222
-library.y = 412
+
 
 local libraryOutline = graphics.newOutline( 2, "library.png" )
 
@@ -27,10 +26,41 @@ local student = display.newImageRect( "student.png", 70, 70 )
 student.x = display.contentCenterX
 student.y = display.contentCenterY
 
+local library = display.newImageRect("library.png", 74, 87)
+library.x = -200
+library.y = 400
+
+local foundationBuilding = display.newImageRect("foundationBuilding.png", 74, 87)
+foundationBuilding.x = 500
+foundationBuilding.y = 330
+
+local house = display.newImageRect("house.png", 74, 87)
+house.x = 100
+house.y = 50
+
+local MedicalSchool = display.newImageRect("MedicalSchool.png", 74, 87)
+MedicalSchool.x = 300
+MedicalSchool.y = 150
+
+local SU = display.newImageRect("SU.png", 74, 87)
+SU.x = -150
+SU.y = 150
+
+
+
+
+
+
+
 --Character physics and building physics-- 
 physics.addBody( student, "dynamic", { bounce=0, angle=0 })
 student.isFixedRotation = true 
-physics.addBody( library, "static")
+physics.addBody( library, "dynamic", {isSensor=0})
+physics.addBody( foundationBuilding, "dynamic", {isSensor=0})
+physics.addBody( house, "dynamic", {isSensor=0})
+physics.addBody( MedicalSchool, "dynamic", {isSensor=0})
+physics.addBody( SU, "dynamic", {isSensor=0})
+
 
 events.generateRandomEvent() 
 
