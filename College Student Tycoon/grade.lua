11e8f37a99c585b0759777864c1b6cbf7928d401
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-04-04 00:39:57 +0100

    Update grade.lua

--- College Student Tycoon/grade.lua
+++ College Student Tycoon/grade.lua
@@ -1,47 +1,29 @@
-local timeSystem = require "timeSystem"
+
 local stats = require "stats"
 
 local grade = { }
-grade.endGrade = " "
 
-function grade.aGrade()
-	if ((timeSystem.day >= 1) and ((stats.intel)>= 90))
-		then grade.endGrade = "A"
+function grade.displayGrade()
+	if (stats.intel>= 90) then
+		grade.gradeText = display.newText("Grade = A", 960, 700 , "Ariel Black" , 50 )
+		grade.gradeText:setFillColor( 1, 0, 0 )
+	
+	elseif (stats.intel >= 70 and stats.intel < 90) then
+		grade.gradeText = display.newText("Grade = B", 960, 700 , "Ariel Black" , 50 )
+		grade.gradeText:setFillColor( 1, 0, 0 )
+	
+	elseif (stats.intel >= 50 and stats.intel < 70) then
+		grade.gradeText = display.newText("Grade = C", 960, 700 , "Ariel Black" , 50 )
+		grade.gradeText:setFillColor( 1, 0, 0 )
+	
+	elseif (stats.intel >= 30 and stats.intel < 50) then
+		grade.gradeText = display.newText("Grade = D", 960, 700 , "Ariel Black" , 50 )
+		grade.gradeText:setFillColor( 1, 0, 0 )
+	
+	elseif (stats.intel < 30) then
+		grade.gradeText = display.newText("Grade = FAIL", 960, 700 , "Ariel Black" , 50 )
+		grade.gradeText:setFillColor( 1, 0, 0 )
 	end
-	return grade.endGrade
-end
-
-function grade.bGrade()
-	if ((timeSystem.day >= 1) and (90 >(stats.intel)>= 70))
-		then grade.endGrade = "B"
-	end
-	return grade.endGrade
-end
-
-function grade.cGrade()
-	if ((timeSystem.day >= 1) and (70 >(stats.intel)>= 50))
-		then grade.endGrade = "C"
-	end
-	return grade.endGrade
-end
-
-function grade.dGrade()
-	if ((timeSystem.day >= 1) and (50 >(stats.intel)>= 30))
-		then grade.endGrade = "D"
-	end
-	return grade.endGrade
-end
-
-function grade.failGrade()
-	if ((timeSystem.day >= 1) and ((stats.intel)< 30))
-		then grade.endGrade = "FAIL"
-	end
-	return grade.endGrade
-end
-
-function grade.display()
-	grade.gradeText = display.newText(grade.endGrade, 960, 770 , native.systemFont , 40 )
-	grade.gradeText:setFillColor( 1, 0, 0 )
 end
 
 return grade
