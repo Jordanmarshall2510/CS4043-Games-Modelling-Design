29838a2ecad25d0ca386870dcb7a2817f99a2440
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-04-05 00:34:30 +0100

    Update stats.lua

--- College Student Tycoon/stats.lua
+++ College Student Tycoon/stats.lua
@@ -1,29 +1,40 @@
 -- Stats --
 local stats = { } 
 
---Money-- 
+function stats.reset()
+	--Money-- 
+	stats.money = 500
+	--Sleep--
+	stats.sleep = math.random(70,100)
+	--Hygeine--
+	stats.hygiene = math.random(70,100) 
+	--Hunger-- 
+	stats.hunger = math.random(70,100) 
+	--Happiness-- 
+	stats.happiness = math.random(70,100)
+	--Intelligence--
+	stats.intel = math.random(70,100)
+	--Display everything, to be called in the main file-- 
 
-stats.money = 500
+		stats.moneyText = display.newText("Money " .. stats.money, 1750, 30, native.systemFont , 40 )
+		stats.moneyText:setFillColor( 0, 0, 0 )
 
---Sleep--
+		stats.sleepText = display.newText("Sleep " .. stats.sleep, 1750, 70, native.systemFont , 40 )
+		stats.sleepText:setFillColor( 0, 0, 0 )
 
-stats.sleep = math.random(70,100)
+		stats.hygieneText = display.newText("Hygiene " .. stats.hygiene, 1750, 110, native.systemFont , 40 )
+		stats.hygieneText:setFillColor( 0, 0, 0 )
 
---Hygeine--
+		stats.hungerText = display.newText("Hunger " .. stats.hunger, 1750, 150, native.systemFont , 40 )
+		stats.hungerText:setFillColor( 0, 0, 0 )
 
-stats.hygiene = math.random(70,100) 
-
---Hunger-- 
-
-stats.hunger = math.random(70,100) 
-
---Happiness-- 
-
-stats.happiness = math.random(70,100)
-
---Intelligence--
-
-stats.intel = math.random(70,100)
+		stats.happinessText = display.newText("Happiness " .. stats.happiness, 1750, 190, native.systemFont , 40 )
+		stats.happinessText:setFillColor( 0, 0, 0 )
+	
+		stats.intelText = display.newText("Intelligence " .. stats.intel, 1750, 230, native.systemFont , 40 )
+		stats.intelText:setFillColor( 0, 0, 0 )	
+	
+end
 
 --Display everything, to be called in the main file-- 
 
@@ -46,18 +57,19 @@ function stats.display()
 	
 	stats.intelText = display.newText("Intelligence " .. stats.intel, 1750, 230, native.systemFont , 40 )
 	stats.intelText:setFillColor( 0, 0, 0 )
+
 end
 
 function stats.clear() 
-
 	stats.moneyText:removeSelf()
 	stats.hungerText:removeSelf()
 	stats.sleepText:removeSelf()
 	stats.hygieneText:removeSelf()
 	stats.happinessText:removeSelf() 
 	stats.intelText:removeSelf()
-	
 end
+
+
 
 function stats.refresh()
 	stats.clear()
