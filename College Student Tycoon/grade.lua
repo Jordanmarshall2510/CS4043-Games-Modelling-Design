83d1671430ffa4ea72840cecb54dbe8738346070
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-04-08 13:53:22 +0100

    Update grade.lua

--- College Student Tycoon/grade.lua
+++ College Student Tycoon/grade.lua
@@ -5,24 +5,24 @@ local grade = { }
 
 function grade.displayGrade()
 	if (stats.intel>= 90) then
-		grade.gradeText = display.newText("Grade = A", 960, 700 , "Ariel Black" , 50 )
-		grade.gradeText:setFillColor( 1, 0, 0 )
+		grade.gradeText = display.newText("Grade = A", 960, 650, "Ariel Black", 50)
+		grade.gradeText:setFillColor(1, 0, 0)
 	
 	elseif (stats.intel >= 70 and stats.intel < 90) then
-		grade.gradeText = display.newText("Grade = B", 960, 700 , "Ariel Black" , 50 )
-		grade.gradeText:setFillColor( 1, 0, 0 )
+		grade.gradeText = display.newText("Grade = B", 960, 650, "Ariel Black", 50)
+		grade.gradeText:setFillColor(1, 0, 0)
 	
 	elseif (stats.intel >= 50 and stats.intel < 70) then
-		grade.gradeText = display.newText("Grade = C", 960, 700 , "Ariel Black" , 50 )
-		grade.gradeText:setFillColor( 1, 0, 0 )
+		grade.gradeText = display.newText("Grade = C", 960, 650, "Ariel Black", 50)
+		grade.gradeText:setFillColor(1, 0, 0)
 	
 	elseif (stats.intel >= 30 and stats.intel < 50) then
-		grade.gradeText = display.newText("Grade = D", 960, 700 , "Ariel Black" , 50 )
-		grade.gradeText:setFillColor( 1, 0, 0 )
+		grade.gradeText = display.newText("Grade = D", 960, 650, "Ariel Black", 50)
+		grade.gradeText:setFillColor(1, 0, 0)
 	
 	elseif (stats.intel < 30) then
-		grade.gradeText = display.newText("Grade = FAIL", 960, 700 , "Ariel Black" , 50 )
-		grade.gradeText:setFillColor( 1, 0, 0 )
+		grade.gradeText = display.newText("Grade = FAIL", 960, 650, "Ariel Black", 50)
+		grade.gradeText:setFillColor(1, 0, 0)
 	end
 end
 
