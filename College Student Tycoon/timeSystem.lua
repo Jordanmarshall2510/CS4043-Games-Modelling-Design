bedadd4e01d3cc4e0159b39cf8a4eeb07cf4df46
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-04-08 13:53:51 +0100

    Update timeSystem.lua

--- College Student Tycoon/timeSystem.lua
+++ College Student Tycoon/timeSystem.lua
@@ -2,21 +2,21 @@ local timeSystem = { }
 --"time" is already used in Lua
 
 function timeSystem.defaultValues()
-	timeSystem.day = 1 
+	timeSystem.day = 1
 	timeSystem.time = 9 -- initial value, game starts at 9 AM
 end 
 
 function timeSystem.text() 
-	timeSystem.inGameClock = display.newText("Day "..timeSystem.day.." - "..timeSystem.time ..":00" , 960, 20, native.systemFont , 40 )
-	timeSystem.inGameClock:setFillColor( 0, 0, 0 )
+	timeSystem.inGameClock = display.newText("Day "..timeSystem.day.." - "..timeSystem.time ..":00", 960, 20, native.systemFont, 40)
+	timeSystem.inGameClock:setFillColor(0, 0, 0)
 	timeSystem.inGameClock.alpha = 0
 end
 
 function timeSystem.advanceTime(n) 
 	timeSystem.time = timeSystem.time + n
-	if timeSystem.time > 24 
+	if timeSystem.time > 23
 	then
-		timeSystem.time = timeSystem.time - 25
+		timeSystem.time = timeSystem.time - 24
 		timeSystem.day = timeSystem.day + 1
 	end
 
