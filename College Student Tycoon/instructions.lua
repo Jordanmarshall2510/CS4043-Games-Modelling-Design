4910b61b42cd20ea5ac379aa30993b766296e32f
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-04-09 18:55:16 +0100

    Update instructions.lua

--- College Student Tycoon/instructions.lua
+++ College Student Tycoon/instructions.lua
@@ -1,33 +1,32 @@
-local composer = require( "composer" )
+local composer = require("composer")
 local game = require("game")
  
 local instructions = composer.newScene()
 
-function instructions:show( event )
+function instructions:show(event)
     local sceneGroup = self.view
     local phase = event.phase
  
-    if ( phase == "will" ) then
-	display.newImage( sceneGroup, "instructions.png", display.contentCenterX, display.contentCenterY , native.systemFont , 60 )
+    if (phase == "will") then
+		display.newImage( sceneGroup, "instructions.png", display.contentCenterX, display.contentCenterY)
 
-	elseif ( phase == "did" ) then 
-	playButton = display.newImageRect( sceneGroup, "playButton.png", 200, 75)
-	playButton.x = 1700 
-	playButton.y = 1000
-	playButton.name = "play" 
+	elseif (phase == "did") then 
+		nextButton = display.newImageRect( sceneGroup, "nextButton.png", 200, 75)
+		nextButton.x = 1700 
+		nextButton.y = 1000
 	
-	playButton:addEventListener("touch", startGame)
+		nextButton:addEventListener("touch", startGame)
 	end
 end
 	
 function startGame(event)
-	composer.gotoScene( "game", {effect = "fade", time = 700} )
-	playButton:removeEventListener("touch", startGame)
+	composer.gotoScene("game", {effect = "fade", time = 700})
+	nextButton:removeEventListener("touch", startGame)
 end
 
-instructions:addEventListener( "create", instructions )
-instructions:addEventListener( "show", instructions )
-instructions:addEventListener( "hide", instructions )
-instructions:addEventListener( "destroy", instructions )
+instructions:addEventListener("create", instructions)
+instructions:addEventListener("show", instructions)
+instructions:addEventListener("hide", instructions)
+instructions:addEventListener("destroy", instructions)
 
 return instructions 
