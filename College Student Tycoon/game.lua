074a679b4bdcaec17a4a2d5660b4463fc30232ba
mikfar <45609856+mikfar@users.noreply.github.com>
2019-04-10 15:37:00 +0100

    Update game.lua

--- College Student Tycoon/game.lua
+++ College Student Tycoon/game.lua
@@ -170,7 +170,6 @@ function game:show(event)
 	end
 	
 function game:destroy(event)
- 
     local sceneGroup = self.view
     timeSystem.defaultValues()
     timeSystem.hide() 
@@ -182,7 +181,7 @@ end
 function endGame(event) 
 	if(stats.sleep <= 0 or stats.hygiene <= 0 or stats.hunger <= 0 or stats.happiness <= 0 or stats.intel <= 0 or timeSystem.day == 14) then
 		composer.removeScene("game") 
-		composer.gotoScene("gameOver", {effect = "fromRight", time = 500}) 
+		composer.gotoScene("gameOver", {effect = "fade", time = 500}) 
 	end 
 end 
 
