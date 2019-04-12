ec9dd798e62e624feb2af2bea8cfc92c89987d2f
mikfar <45609856+mikfar@users.noreply.github.com>
2019-04-12 15:30:30 +0100

    Update stats.lua

--- College Student Tycoon/stats.lua
+++ College Student Tycoon/stats.lua
@@ -22,21 +22,21 @@ function stats.text()
 	stats.group = display.newGroup()
 	
 	stats.moneyText = display.newText( stats.group, "Money " .. stats.money, 1750, 30, native.systemFont , 40 )
-	if stats.money < 50 then 
+	if stats.money <= 100 then 
 		stats.moneyText:setFillColor( 255, 0, 0 )
 	else 
 		stats.moneyText:setFillColor( 0, 0, 0 )
 	end
 	
 	stats.sleepText = display.newText( stats.group, "Sleep " .. stats.sleep, 1750, 70, native.systemFont , 40 )
-	if stats.sleep < 20 then 
+	if stats.sleep <= 20 then 
 		stats.sleepText:setFillColor( 255, 0, 0 )
 	else 
 		stats.sleepText:setFillColor( 0, 0, 0 )
 	end
 
 	stats.hygieneText = display.newText( stats.group, "Hygiene " .. stats.hygiene, 1750, 110, native.systemFont , 40 )
-	if stats.hygiene < 20 then 
+	if stats.hygiene <= 20 then 
 		stats.hygieneText:setFillColor( 255, 0, 0 )
 	else 
 		stats.hygieneText:setFillColor( 0, 0, 0 )
@@ -44,21 +44,21 @@ function stats.text()
 
 	stats.hungerText = display.newText( stats.group, "Hunger " .. stats.hunger, 1750, 150, native.systemFont , 40 )
 	stats.hungerText:setFillColor( 0, 0, 0 )
-	if stats.hunger < 20 then 
+	if stats.hunger <= 20 then 
 		stats.hungerText:setFillColor( 255, 0, 0 )
 	else 
 		stats.hungerText:setFillColor( 0, 0, 0 )
 	end
 
 	stats.happinessText = display.newText( stats.group, "Happiness " .. stats.happiness, 1750, 190, native.systemFont , 40 )
-	if stats.happiness < 20 then 
+	if stats.happiness <= 20 then 
 		stats.happinessText:setFillColor( 255, 0, 0 )
 	else 
 		stats.happinessText:setFillColor( 0, 0, 0 )  
 	end
 	
 	stats.intelText = display.newText( stats.group, "Intelligence " .. stats.intel, 1750, 230, native.systemFont , 40 )
-	if stats.intel < 20 then 
+	if stats.intel <= 20 then 
 		stats.intelText:setFillColor( 255, 0, 0 )
 	else 
 		stats.intelText:setFillColor( 0, 0, 0 )
